e39a6b2a810e3f0c3e24e3cf18c8861453f6357a
1351618 <1351618@gmail.com>
2024-10-15 17:10:39 +0300

    test 4

--- src/js/words.js
+++ src/js/words.js
@@ -10007,12 +10007,22 @@ const words = [
 const words_sect = document.getElementById("words");
 
 const words_content = `
-    <ul class="word-list">
-    ${words
-      .map((word, index) => {
-        return `<li><input type="checkbox">${index + 1} ${word}</li>`;
-      })
-      .join("")}
-    </ul>
+${Array.from({ length: Math.ceil(words.length / 100) }, (_, i) => {
+  const start = i * 100;
+  const end = start + 100;
+  const listItems = words
+    .slice(start, end)
+    .map((word, index) => {
+      const liIndex = start + index + 1; // Индекс для li
+      // Определяем, нужно ли вставлять <br> после каждого 10-го элемента
+      const br =
+        (index + 1) % 10 === 0 && (start + index + 1) % 100 !== 0 ? "<br>" : "";
+      return `<li><input type="checkbox">${liIndex} ${word}</li>${br}`;
+    })
+    .join("");
+
+  return `<ul>${listItems}</ul>`;
+}).join("")}
 `;
+
 words_sect.innerHTML = words_content;
