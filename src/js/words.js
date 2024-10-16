b0456cc63f568dc43546cc1371b186ce531e046c
1351618 <1351618@gmail.com>
2024-10-16 17:22:55 +0300

    test 6

--- src/js/words.js
+++ src/js/words.js
@@ -6,54 +6,134 @@ fetch(wordsData) //data -'https/...'
   .then((data) => outputToPage(data))
   .catch((error) => console.log("error"));
 
+// function outputToPage(data) {
+//   const numbOfTransl = Object.keys(data[1].lang);
+//   console.log(numbOfTransl);
+//   let checkboxStates = {};
+//   const words_content = `
+//   <div class="words__lang-switch">${numbOfTransl
+//     .map((val) => {
+//       return `<label>
+//       <input type="checkbox" onchange="updateCheckboxState('${val}', this.checked)"> ${val}
+//     </label>`;
+//     })
+//     .join("")}</div>
+//   <div class="words__cont">
+//     ${Array.from(
+//       { length: Math.ceil(Object.keys(data).length / 100) },
+//       (_, i) => {
+//         const start = i * 100;
+//         const end = start + 100;
+//         const listItems = Object.keys(data)
+//           .slice(start, end)
+//           .map((key, index) => {
+//             const liIndex = start + index + 1; // Индекс для li
+//             // Определяем, нужно ли вставлять <br> после каждого 10-го элемента
+//             const br =
+//               (index + 1) % 10 === 0 && (start + index + 1) % 100 !== 0
+//                 ? "<br>"
+//                 : "";
+//             return `<li>
+//                 <input type="checkbox">
+//                 <i>${liIndex}</i>
+//                 <b>${data[key].word}</b>
+//                 ${
+//                   numbOfTransl.length === 0
+//                     ? ``
+//                     : `${numbOfTransl
+//                         .map((val) => {
+//                           return `<p class=${
+//                             checkboxStates.val ? "active" : ""
+//                           }>${data[key].lang[val]}</p>`;
+//                         })
+//                         .join("")}`
+//                 }
+
+//             </li>${br}`;
+//           })
+//           .join("");
+
+//         return `<ul>${listItems}</ul>`;
+//       }
+//     ).join("")}
+//     </div>
+//     `;
+
+//   // Функция для обновления состояния чекбоксов
+//   window.updateCheckboxState = function (language, isChecked) {
+//     checkboxStates[language] = isChecked;
+//     console.log(checkboxStates); // Вывод состояния в консоль для проверки
+//   };
+
+//   words_sect.innerHTML = words_content;
+// }
+
 function outputToPage(data) {
   const numbOfTransl = Object.keys(data[1].lang);
-  console.log(numbOfTransl);
   let checkboxStates = {};
+
   const words_content = `
-  <div class="words__lang-switch">${numbOfTransl
-    .map((val) => {
-      return `<label>
-      <input type="checkbox" onchange="updateCheckboxState('${val}', this.checked)"> ${val}
-    </label>`;
-    })
-    .join("")}</div>
-  <div class="words__cont">
-    ${Array.from(
-      { length: Math.ceil(Object.keys(data).length / 100) },
-      (_, i) => {
-        const start = i * 100;
-        const end = start + 100;
-        const listItems = Object.keys(data)
-          .slice(start, end)
-          .map((key, index) => {
-            const liIndex = start + index + 1; // Индекс для li
-            // Определяем, нужно ли вставлять <br> после каждого 10-го элемента
-            const br =
-              (index + 1) % 10 === 0 && (start + index + 1) % 100 !== 0
-                ? "<br>"
-                : "";
-            return `<li>
+      <div class="words__lang-switch">
+        ${numbOfTransl
+          .map((val) => {
+            return `<label>
+              <input type="checkbox" onchange="updateCheckboxState('${val}', this.checked)"> ${val}
+            </label>`;
+          })
+          .join("")}
+      </div>
+      <div class="words__cont">
+        ${Array.from(
+          { length: Math.ceil(Object.keys(data).length / 100) },
+          (_, i) => {
+            const start = i * 100;
+            const end = start + 100;
+            const listItems = Object.keys(data)
+              .slice(start, end)
+              .map((key, index) => {
+                const liIndex = start + index + 1;
+                const br =
+                  (index + 1) % 10 === 0 && (start + index + 1) % 100 !== 0
+                    ? "<br>"
+                    : "";
+                return `<li>
                 <input type="checkbox">
                 <i>${liIndex}</i>
                 <b>${data[key].word}</b>
-                ${numbOfTransl.length === 0 ? `` : ``}
-          
-            </li>${br}`;
-          })
-          .join("");
+                ${numbOfTransl
+                  .map((val) => {
+                    return `<p class="lang-${val}">${data[key].lang[val]}</p>`;
+                  })
+                  .join("")}
+              </li>${br}`;
+              })
+              .join("");
 
-        return `<ul>${listItems}</ul>`;
-      }
-    ).join("")}
-    </div>
+            return `<ul>${listItems}</ul>`;
+          }
+        ).join("")}
+      </div>
     `;
 
-  // Функция для обновления состояния чекбоксов
+  // Обновляем состояния чекбоксов
   window.updateCheckboxState = function (language, isChecked) {
     checkboxStates[language] = isChecked;
-    console.log(checkboxStates); // Вывод состояния в консоль для проверки
+    updateLanguagesDisplay(language, isChecked);
   };
+
+  // Функция для обновления отображения языков
+  function updateLanguagesDisplay(language, isChecked) {
+    const elements = document.querySelectorAll(`.lang-${language}`);
+    elements.forEach((el) => {
+      if (isChecked) {
+        el.classList.add("active");
+      } else {
+        el.classList.remove("active");
+      }
+    });
+  }
 
   words_sect.innerHTML = words_content;
 }
+
+outputToPage(data);
